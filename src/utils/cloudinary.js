ba67c1a4b9554bc8cd73475aa14b03793af2a0e2
Pranjal Kumar <pranjalkumar02062004@gmail.com>
2026-06-15 01:43:35 +0530

    file uploding done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary= async (localFilePath)=>{
+     try{
+        if(!localFilePath)return null;
+        const response =await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        });
+        console.log(`File uploaded on cloudinary successfully available at ${response.url}`);
+        return response;
+     }
+     catch(err){
+          fs.unlinkSync(localFilePath);
+          console.log(`File upload on cloudinay is unsuccessfull`);
+          return null;
+     }
+}
+
+export {uploadOnCloudinary}
