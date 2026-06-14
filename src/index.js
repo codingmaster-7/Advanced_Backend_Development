b3efe28675faedec88841d0981d74de378065a05
Pranjal Kumar <pranjalkumar02062004@gmail.com>
2026-06-14 22:34:04 +0530

    utils ready

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
-import dns from 'node:dns';
-dns.setDefaultResultOrder('ipv4first');
 import 'dotenv/config';
 import connectionDb from "./db/index.js"
+import {app} from './app.js'
 
-connectionDb();
+connectionDb()
+.then(app.listen(process.env.PORT||8000,()=>{
+     console.log(`App is running on port ${process.env.PORT}`);
+}))
+.catch((err)=>{
+    console.log("MongoDB connection failed error : ",err);
+})
 
-// const app=express();
-// (async ()=>{
-//     try{
-//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//       app.on("Error",(error)=>{
-//          console.log("Error",error);
-//          throw error;
-//       })
-//       app.listen(process.env.PORT,()=>{
-//           console.log(`App is listenig on port${process.env.PORT}`); 
-//       })
-//     }
-//     catch (error){
-//         console.log("Error : ",error)
-//         throw error
-//     }
-// })()
